f1512f40177af69ec7eb552ae3e8a2701da319e5
florianempinet <fempinet@gmail.com>
2020-09-28 16:25:48 +0200

    Modif

--- exo21.rb
+++ exo21.rb
@@ -1,9 +1,11 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 
+puts "Choisis un nombre :) "
+
 usernumber = gets.chomp.to_i
 
 pyramidnumber = 1
 while pyramidnumber <= usernumber
-  puts ("# " * pyramidnumber).rjust(20)
+  puts ("#" * pyramidnumber).rjust(usernumber)
   pyramidnumber += 1
 end
