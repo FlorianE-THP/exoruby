87c7ac3463e897dfee63cd95b6aa3eee7965a8c0
florianempinet <fempinet@gmail.com>
2020-09-29 13:43:03 +0200

    Modif Array

--- exo18.rb
+++ exo18.rb
@@ -6,10 +6,12 @@ puts "Veux-tu créer 50 adresse mail, répond oui ou non ?"
 case (gets.chomp)
 when 'oui'
   for i in 0..50
-    puts "jean.dupont.%02d@gmail.com" %[i]
+    mail.push("jean.dupont.%02d@gmail.com" %[i])
   end
 when 'non,'
   puts "Ok.. tant pis"
 else
   puts "Repond simplement par oui ou par non stp.."
 end
+
+puts mail
--- exo19.rb
+++ exo19.rb
@@ -1,17 +1,23 @@
 mail = []
+mail2 = []
 numbertochange = 00
 
 puts "Veux-tu créer 50 adresse mail, répond oui ou non ?"
 
 case (gets.chomp)
 when 'oui'
-  (1..50).each do |i|
-    if i % 2 == 0
-    puts "jean.dupont.%02d@gmail.com" %[i]
-  end
+  50.times do |i|
+    i += 1
+    mail.push("jean.dupont.%02d@gmail.com" %[i])
+    if i.even?
+      mail2 << "jean.dupont.%02d@email.fr" %[i]
 end
+end
+
 when 'non,'
   puts "Ok.. tant pis"
 else
   puts "Repond simplement par oui ou par non stp.."
 end
+
+puts mail2
